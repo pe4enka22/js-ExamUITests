71a3e8f7eca6d7a140c2c7217081dbc5747c2430
pe4enka22 <missis.bug@gmail.com>
2024-03-02 00:09:09 +0200

    finish order tests

--- cypress/e2e/orderItemTests.cy.js
+++ cypress/e2e/orderItemTests.cy.js
@@ -1,17 +1,19 @@
 import user from '../fixtures/user.json'
 import {findProduct} from '../support/helper'
 import loginPage from "../support/pages/LoginPage";
-import BasePage from "../support/pages/BasePage";
 import orderPage from "../support/pages/OrderPage";
 
 describe('Order suite', () => {
-  it('Order one product from homepage', () => {
+  it.only('Order one product from homepage', () => {
     loginPage.visit();
     loginPage.fillLoginFields(user.email, user.password);
     loginPage.getSubmitButton().click();
 
     findProduct('Banana Juice (1000ml)');
+
     orderPage.getYourBasketButton().click();
+    cy.get('.cdk-cell.cdk-column-product.mat-column-product').should('have.text', ' Banana Juice (1000ml) ');
+    cy.get('.content-align.cdk-column-quantity.mat-column-quantity>span').should('have.text', ' 1');
 
     orderPage.getCheckoutButton().click();
     cy.get('#mat-radio-40').click();
@@ -20,7 +22,12 @@ describe('Order suite', () => {
     orderPage.getDeliveryContinueButton().click();
     cy.get(' #mat-radio-44').click();
     cy.get('[aria-label="Proceed to review"]').click();
+    cy.get('.cdk-cell.cdk-column-product.mat-column-product').should('have.text', ' Banana Juice (1000ml) ');
+    cy.get('.content-align.cdk-column-quantity.mat-column-quantity>span').should('have.text', ' 1');
+
     cy.get('[aria-label="Complete your purchase"]').click();
+    cy.get('.cdk-cell.cdk-column-product.mat-column-product').should('have.text', 'Banana Juice (1000ml) ');
+    cy.get('.cdk-cell.cdk-column-quantity.mat-column-quantity>span').should('have.text', '1');
     cy.get('.confirmation').should('have.text', "Thank you for your purchase!")
   })
 
@@ -32,8 +39,19 @@ describe('Order suite', () => {
 
     findProduct('Banana Juice (1000ml)');
     findProduct('Banana Juice (1000ml)');
-    findProduct('Eggfruit Juice (500ml) ');
+    findProduct('Eggfruit Juice (500ml)');
+
+
     orderPage.getYourBasketButton().click();
+    cy.get('.cdk-cell.cdk-column-product.mat-column-product').first().should('have.text', ' Banana Juice (1000ml) ');
+    cy.get('.content-align.cdk-column-quantity.mat-column-quantity>span').first().should('have.text', ' 2');
+    cy.get('.cdk-cell.cdk-column-product.mat-column-product').last().should('have.text', ' Eggfruit Juice (500ml) ');
+    cy.get('.content-align.cdk-column-quantity.mat-column-quantity>span').last().should('have.text', ' 1');
+
+    cy.get('[data-icon="minus-square"]').first().click();
+    cy.get('.content-align.cdk-column-quantity.mat-column-quantity>span').first().should('have.text', ' 1');
+    cy.get('[data-icon="plus-square"]').last().click();
+    cy.get('.content-align.cdk-column-quantity.mat-column-quantity>span').last().should('have.text', ' 2');
 
 
     orderPage.getCheckoutButton().click();
@@ -43,8 +61,19 @@ describe('Order suite', () => {
     orderPage.getDeliveryContinueButton().click();
     cy.get(' #mat-radio-44').click();
     cy.get('[aria-label="Proceed to review"]').click();
+
+    cy.get('.cdk-cell.cdk-column-product.mat-column-product').first().should('have.text', ' Banana Juice (1000ml) ');
+    cy.get('.content-align.cdk-column-quantity.mat-column-quantity>span').first().should('have.text', ' 1');
+    cy.get('.cdk-cell.cdk-column-product.mat-column-product').last().should('have.text', ' Eggfruit Juice (500ml) ');
+    cy.get('.content-align.cdk-column-quantity.mat-column-quantity>span').last().should('have.text',' 2');
+
+
     cy.get('[aria-label="Complete your purchase"]').click();
     cy.get('.confirmation').should('have.text', "Thank you for your purchase!")
+    cy.get('.cdk-cell.cdk-column-product.mat-column-product').first().should('have.text', 'Banana Juice (1000ml) ');
+    cy.get('.cdk-cell.cdk-column-quantity.mat-column-quantity>span').first().should('have.text', '1');
+    cy.get('.cdk-cell.cdk-column-product.mat-column-product').last().should('have.text', 'Eggfruit Juice (500ml) ');
+    cy.get('.cdk-cell.cdk-column-quantity.mat-column-quantity>span').last().should('have.text','2');
 
   })
 })
--- cypress/support/helper.js
+++ cypress/support/helper.js
@@ -1,9 +1,3 @@
-//export function findProduct(productName) {
-  //  cy.get('body').find(`[alt="${productName}"]`).click('[aria-label="Add to Basket"]');
-//}
-
-
 export function findProduct(productName) {
-    cy.contains(productName)
-        .parents('.mat-card').find('[aria-label="Add to Basket"]').click();
+    cy.contains(productName).parents('.mat-card').find('[aria-label="Add to Basket"]').click();
 }
